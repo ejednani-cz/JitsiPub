aa8ff6ae7cb116286a6662d8a795dc44c59a8c1f
Ondřej Kotas <ok@krtkovo.eu>
2020-03-21 03:26:52 +0100

    release

---
+++ js/planek.js
@@ -0,0 +1,15 @@
+mapka_base=new Image();
+mapka_vlnena=new Image();
+mapka_kuchyne=new Image();
+mapka_semneles=new Image();
+mapka_hajzle=new Image();
+mapka_agrofet=new Image();
+mapka_48=new Image();
+    
+mapka_base.src="images/base.png";
+mapka_vlnena.src="images/vlnena.png";
+mapka_kuchyne.src="images/kuchyne.png";
+mapka_semneles.src="images/semneles.png";
+mapka_hajzle.src="images/hajzle.png";
+mapka_agrofet.src="images/agrofet.png";
+mapka_48.src="images/48.png";
